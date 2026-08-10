a99203620cefc1e05ab2a683c18c517dfac27dd7
Jack Poupore <jack.poupore90@gmail.com>
2026-08-10 09:50:35 -0700

    Use verified skimstash.com domain for outbound email

--- app/email.py
+++ app/email.py
@@ -11,7 +11,7 @@ import os
 import requests
 
 RESEND_API_URL = "https://api.resend.com/emails"
-FROM_ADDRESS = "Skim <onboarding@resend.dev>"
+FROM_ADDRESS = "Skim <noreply@skimstash.com>"
 
 
 class EmailSendError(Exception):
